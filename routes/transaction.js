7f68193cdd35d8e8f776f936fa39dc371919e35f
selvamurugaiah <selvamurugaiah100@gmail.com>
2023-06-09 13:00:22 +0530

    route setup

--- routes/transaction.js
+++ routes/transaction.js
@@ -4,6 +4,9 @@ const { addIncome, getIncomes, deleteIncomes } = require('../controllers/income'
 const router = require('express').Router()
 
 //Income
+router.get('/', (req,res)=>{
+      res.send("PettyCash-Manager")
+})
 router.post('/add-income', addIncome)
 router.get('/get-incomes',getIncomes)
 router.delete('/delete-income/:id',deleteIncomes)
