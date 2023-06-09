a7d8135a7bfb21e3317cb7833c9602a8e8c792e9
selvamurugaiah <selvamurugaiah100@gmail.com>
2023-06-09 13:02:24 +0530

    new

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors())
 
 //routes
 
-readdirSync('./routes').map((route) => app.use('/',require('./routes/' + route)))
+readdirSync('./routes').map((route) => app.use('/api/v1',require('./routes/' + route)))
 
 const server =()=>{
     db()
